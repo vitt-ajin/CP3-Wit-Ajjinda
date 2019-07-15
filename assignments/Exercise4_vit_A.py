5829306d72ec9daecceebedbfde015037050b504
vitt-ajin <glofint.arm@gmail.com>
2019-07-15 13:23:28 +0700

    Add Exercise4

---
+++ assignments/Exercise4_vit_A.py
@@ -0,0 +1,9 @@
+Fo_eng = 60
+gene_busi =  75
+intro_com =80
+comp_pro = 75
+print("--- Your Score ---")
+print("Foundation English : ",Fo_eng)
+print("General Business : ",gene_busi)
+print("Introduction to Computer Systems : ",intro_com)
+print("Computer Programming : ",comp_pro)
