08f453551dab919ad32d8a49a7b6d897ad30c374
Vit-Ajin <glofint.arm@gmail.com>
2019-07-20 18:26:12 +0700

    add Lc73Coll

---
+++ assignments/Lc73Coll_Vit_A,.py
@@ -0,0 +1,16 @@
+menuList=[]
+def showBill():
+    temp = 0
+    print("-----My Shop-----")
+    for number in range(len(menuList)):
+        print(menuList[number])
+        temp+=int(menuList[number][1])
+    print("Total :",temp)
+while True:
+    menuName=input("Please Enter :")
+    if(menuName.lower()=="exit"):
+        break
+    else:
+        menuPrice=input("Price :")
+        menuList.append([menuName,menuPrice])
+showBill()
